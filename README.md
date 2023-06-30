05756e996ef34a26e33779a90765a8ba4b371b6e
Kirans-143 <kiran.shet77@gmail.com>
2023-06-30 18:15:52 +0530

    Added: Header, LeftNav, Feed, VideoCard for YouTube_Clone

---
+++ README.md
@@ -0,0 +1,4 @@
+YouTube Clone using
+1.React.js(18),
+2.Taiwind CSS(to build UI)
+3.Rapid API (to fetch youtube data)
---
+++ src/components/VideoDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoDetails = () => {
+  return <div>Hello Kiran</div>;
+};
+
+export default VideoDetails;
